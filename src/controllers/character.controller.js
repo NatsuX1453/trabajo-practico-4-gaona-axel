2f40b4df14825980ed5c073c00f9ac36c80b4554
axelon <207878001+NatsuX1453@users.noreply.github.com>
2025-06-25 23:30:00 -0300

    se agregaron mas controladores

--- src/controllers/character.controller.js
+++ src/controllers/character.controller.js
@@ -38,6 +38,36 @@ try {
 	if (nombreUnico !== null) return res.status(400).json({ message: 'Nombre ya existe' });
 
 	const character = await Character.create({ name, ki, race, gender, description });
+
+	res.status(201).json({ message: 'Personaje creado exitosamente', character });
 } catch (error) {
 	res.status(500).json({ message: error.message });
 }
+
+export const actualizarPersonaje = async (req, res) => {
+	const { name, ki, race, gender, description } = req.body;
+
+	if (req.body) {
+		for (let valor in req.body) {
+			if (typeof valor === 'string') {
+				req.body[valor] = req.body[valor].trim();
+			}
+		}
+	}
+
+	try {
+		const nombreUnico = await Character.findOne({ where: { name } });
+		if (nombreUnico !== null)
+			return res.status(400).json({ message: 'Nombre ya existe' });
+
+		const [updated] = await Character.update(name, ki, race, gender, description, {
+			where: { id: req.params.id },
+		});
+
+		if (updated.length === 0)
+			return res.status(404).json({ message: 'Personaje no encontrado' });
+		return res.status(200).json({ message: 'Personaje actualizado exitosamente' });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
