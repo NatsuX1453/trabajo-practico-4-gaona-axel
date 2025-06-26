c363051037811380dfdae295c43a7dfa4db61365
axelon <207878001+NatsuX1453@users.noreply.github.com>
2025-06-25 21:16:21 -0300

    se completaron las condiguraciones del models

--- src/models/character.model.js
+++ src/models/character.model.js
@@ -1,5 +1,6 @@
 import { sequelize } from '../config/database.js';
 import { DataTypes } from 'sequelize';
+
 const Character = sequelize.define('character', {
 	id: {
 		type: DataTypes.INTEGER,
@@ -10,9 +11,22 @@ const Character = sequelize.define('character', {
 		type: DataTypes.STRING,
 		allowNull: false,
 	},
-	age: {
+	ki: {
 		type: DataTypes.INTEGER,
 		allowNull: false,
 	},
+	race: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	gender: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	description: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
 });
+
 export default Character;
