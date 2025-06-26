9b0cbd41818d4832fb7a1f3ac69319e008f91e05
axelon <207878001+NatsuX1453@users.noreply.github.com>
2025-06-26 14:14:07 -0300

    ,

--- src/routes/character.route.js
+++ src/routes/character.route.js
@@ -2,15 +2,11 @@ import express from 'express';
 import {
 	CreateCharacter,
 	deleteCharacter,
-	// getALLCharacters,
-	// getCharacterById,
 	actualizarPersonaje,
 } from '../controllers/character.controller.js';
 const router = express.Router();
 
 router.post('/characters', CreateCharacter);
-// router.get('/characters', getALLCharacters);
-// router.get('/characters/:id', getCharacterById);
 router.put('/characters/:id', actualizarPersonaje);
 router.delete('/characters/:id', deleteCharacter);
 
